c9c6d18e83e1d204b63560e8bce9ee4f1141277b
Manab <getbubun@gmail.com>
2020-01-04 03:32:07 +0530

    more to complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-  title = 'crypto-website';
+export class AppComponent implements OnInit {
+  title = "crypto-website";
+
+  contactForm: FormGroup;
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.createContactForm();
+  }
+
+  createContactForm() {
+    this.contactForm = this.fb.group({
+      name: ["", Validators.required, Validators.min(3), Validators.max(32)],
+      email: ["", Validators.required],
+      company: ["", Validators.required, Validators.pattern(/^[\w\s]+$/)],
+      subject: ["", Validators.required, Validators.min(0), Validators.max(15)],
+      message: [
+        "",
+        Validators.required,
+        Validators.min(30),
+        Validators.max(150)
+      ]
+    });
+  }
 }
